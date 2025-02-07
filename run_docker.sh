28bc9d1c03e13702e6f06df20f79381d39403835
Anton P <anton.polishko@gmail.com>
2025-02-07 09:00:13 +0000

    update run_docker to mount current dir

--- run_docker.sh
+++ run_docker.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run --name mergekit -it --rm --device /dev/kfd --device /dev/dri/renderD128 --security-opt seccomp=unconfined mergekit-evolve:latest bash
+docker run --name mergekit -it --rm --device /dev/kfd --device /dev/dri/renderD128 --security-opt seccomp=unconfined -v $(pwd):/app mergekit-evolve:latest bash
